f1e50d68ce512233765e629e054fc1447eeb6aaa
Benjamin Cassinat <benftwc@gmail.com>
2013-11-18 21:36:33 +0100

    First files added

---
+++ fit-yt.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Benftwc
+ * Date: 18/11/13
+ * Time: 21:04
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Benftwc
+ * Date: 18/11/13
+ * Time: 20:59
+ * To change this template use File | Settings | File Templates.
+ */
