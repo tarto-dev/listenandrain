6a973f42aa1036169122d49886d39047077e4074
Benjamin Cassinat <benftwc@gmail.com>
2013-11-18 23:02:19 +0100

    It's finaly works ;)

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $vid = (isset($_GET['v'])) ? $_GET['v'] : NULL;
 
         ?>
 
-            <iframe id="iframe" src="http://www.youtube.com/embed/<?php echo $vid; ?>?rel=0&amp;hd=1&amp;autoplay=1&amp;controls=2" frameborder="0" allowfullscreen="" style="width: 855px; height: 511px;"></iframe>
+            <iframe id="iframe" src="http://www.youtube.com/embed/<?php echo $vid; ?>?rel=0&amp;hd=1&amp;autoplay=1&amp;controls=0" frameborder="0" allowfullscreen="" style="width: 855px; height: 511px;"></iframe>
             <h3>Video informations <br />
 
                 <small>
@@ -55,7 +55,12 @@ $vid = (isset($_GET['v'])) ? $_GET['v'] : NULL;
 
             </h3>
             <div class="controllers">
-                Repeater : <input type="checkbox" class="repeater" />
+                Repeater : <input type="checkbox" class="repeater" /> <span class="repeater-counter">1</span> listens<br />
+                Mood : <span class="pause">Pause</span><span class="play" style="display: none;">Play</span> ( Volume :
+                            <span class="quarter">1/3</span>
+                            <span class="half" style="display: none">2/3</span>
+                            <span class="full" style="display: none">3/3</span>
+                )
             </div>
 
 
@@ -63,16 +68,61 @@ $vid = (isset($_GET['v'])) ? $_GET['v'] : NULL;
         <script>
 
             $("#iframe").ready(function () {
-                setTimeout(function(){
+                var plays = 1;
+                setInterval(function(){
                     if ($(".repeater").is(':checked')) {
                         $('#iframe').attr("src", $('#iframe').attr("src"));
-                    } else {
-                        console.log('tour +1');
+                        plays++;
+                        $('.repeater-counter').text(plays);
                     }
-                }, 10000);
-                console.log('pouet');
+                }, <?php echo (int)$video->length*1000; ?>);
             });
 
+            $(document).ready(function() {
+                var audioElement = document.createElement('audio');
+                audioElement.setAttribute('src', 'audio.mp3');
+                audioElement.setAttribute('autoplay', 'autoplay');
+                audioElement.setAttribute('loop', 'true');
+                //audioElement.load()
+
+                $.get();
+
+                audioElement.addEventListener("load", function() {
+                    audioElement.play();
+                    audioElement.volume=1;
+                }, true);
+
+
+                $('.quarter').click(function() {
+                    audioElement.volume=0.33;
+                    $('.quarter').hide();
+                    $('.half').show();
+                });
+
+                $('.half').click(function() {
+                    audioElement.volume=0.66;
+                    $('.half').hide();
+                    $('.full').show();
+                });
+
+                $('.full').click(function() {
+                    audioElement.volume=1;
+                    $('.full').hide();
+                    $('.quarter').show();
+                });
+
+                $('.play').click(function() {
+                    $('.pause').show();
+                    $('.play').hide();
+                    audioElement.play();
+                });
+                $('.pause').click(function() {
+                    $('.play').show();
+                    $('.pause').hide();
+                    audioElement.pause();
+                });
+                console.log(audioElement);
+            });
         </script>
 
 
